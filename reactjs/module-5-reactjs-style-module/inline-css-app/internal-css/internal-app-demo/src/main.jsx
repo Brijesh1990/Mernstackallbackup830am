4780903bc707d0f2fa0970573f1e2865f73a4fc1
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-06-30 16:48:01 +0530

    Delete reactjs/module-5-reactjs-style-module/inline-css-app directory

--- reactjs/module-5-reactjs-style-module/inline-css-app/internal-css/internal-app-demo/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import EmployeeApp from './components/EmployeeDetails';
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <EmployeeApp />
-  </StrictMode>
-);
--- reactjs/module-5-reactjs-style-module/inline-css-app/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import InlineApp from './components/InlineApp';
-createRoot(document.getElementById('app')).render(
-  <StrictMode>
-    <InlineApp />
-  </StrictMode>
-);
