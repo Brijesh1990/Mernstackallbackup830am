ba3761cf66c63cdf4b3739aab1855e6dc8fc643e
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-09-11 19:13:03 +0530

    Add files via upload

---
+++ reactjs/module-7-hooks-mui-api-local-storage/new-task-manager/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Layout from './Layout'
+import ContactUs from './ContactUs'
+import Feedback from './Feedback'
+import PageNotFound from './PageNotFound'
+// admin routing
+import Login from './admin/Login'
+import AdminLayout from './AdminLayout'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/feedback' element={<Feedback />} />
+        <Route path='/admin-login' element={<Login />} />
+        <Route path='/admin-login/dashboard' element={<AdminLayout />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
